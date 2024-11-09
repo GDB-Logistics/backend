c257ced06080b6ac0bffb94814bfe5d6e63767f9
GergoKovacs0 <c9kovger@vasvari.org>
2024-11-09 16:07:55 +0100

    implemeted express service

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import { WebSocketServer, WebSocket } from "ws";
 import express from "express";
 
-const PORT = 8080;
-const app = express()
-const wss = new WebSocketServer({ port: PORT });
+import api from "../routes/api";
 
-console.log(`WebSocket server is running on ws://localhost:${PORT}`);
+const EXPRESSPORT = 3000;
+const WSPORT = 8080;
+const app = express();
+const wss = new WebSocketServer({ port: WSPORT });
+
+console.log(`WebSocket server is running on ws://localhost:${WSPORT}`);
 
 wss.on("connection", (ws: WebSocket) => {
   console.log("New client connected");
+
   ws.send("Welcome to my websocket!");
+
   ws.on("message", (message: string) => {
     console.log(`Recived: ${message}`);
   });
@@ -17,4 +22,10 @@ wss.on("connection", (ws: WebSocket) => {
   ws.on("close", () => {
     console.log("Client disconnected");
   });
+});
+
+app.use("/api", api);
+
+app.listen(EXPRESSPORT, () => {
+  console.log(`server listens on port http://localhost:${EXPRESSPORT}`);
 });
