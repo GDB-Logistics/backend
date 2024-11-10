91bdbff6e27d3f744e07afa0b26d3b3b539fd017
GergoKovacs0 <c9kovger@vasvari.org>
2024-11-10 18:03:58 +0100

    created balacner

---
+++ src/model/data.ts
@@ -0,0 +1,14 @@
+const works = [new Array<string>(), new Array<string>(), new Array<string>()];
+
+export const pushNewWork = (work: string) => {
+  const pendingWork1 = works[0].length;
+  const pendingWork2 = works[1].length;
+  const pendingWork3 = works[2].length;
+
+  const workLoads = [pendingWork1, pendingWork2, pendingWork3];
+
+  const smalestWorkLoad = [pendingWork1, pendingWork2, pendingWork3].sort()[0];
+
+  works[smalestWorkLoad].push(work);
+  return smalestWorkLoad;
+};
