7bd916ef365945f49d928bc7cb548aaec13cb686
GergoKovacs0 <c9kovger@vasvari.org>
2024-11-10 18:34:52 +0100

    formated

--- src/model/data.ts
+++ src/model/data.ts
@@ -1,6 +1,10 @@
-const works = [new Array<string>(), new Array<string>(), new Array<string>()];
+const works = [
+  new Array<string>(),
+  new Array<string>(),
+  new Array<string>(),
+];
 
-export const pushNewWork = (work: string) => {
+export const pushNewWork = (work: string): number => {
   const pendingWork1 = works[0].length;
   const pendingWork2 = works[1].length;
   const pendingWork3 = works[2].length;
@@ -9,6 +13,6 @@ export const pushNewWork = (work: string) => {
 
   const smalestWorkLoad = [pendingWork1, pendingWork2, pendingWork3].sort()[0];
 
-  works[smalestWorkLoad].push(work);
+  works[workLoads.indexOf(smalestWorkLoad)].push(work);
   return smalestWorkLoad;
 };
