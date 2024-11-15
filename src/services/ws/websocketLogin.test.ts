ee4efa63e1f5665453dcbd36d5387ca94af7b46f
GergoKovacs0 <c9kovger@vasvari.org>
2024-11-16 00:13:12 +0100

    added test to test for login feture

--- src/services/ws/websocketLogin.test.ts
+++ src/services/ws/websocketLogin.test.ts
@@ -1,37 +1,63 @@
 import { Server } from 'socket.io';
-import { createServer } from 'http';
 import Client from 'socket.io-client';
-import { setupWebSocketServer } from '../../wsServer';
+import { handleConnection } from '../ws/websocketService';
 
-describe('WebSocket Login Tests', () => {
-    let io: Server;
-    let serverSocket: any;
+describe('WebSocket Server', () => {
+    const io: Server = new Server();
     let clientSocket: any;
-    const PORT = 3060;
+    const PORT = 9000;
 
     beforeAll((done) => {
-        const httpServer = createServer();
-        io = new Server(httpServer);
-        setupWebSocketServer(httpServer);
-        httpServer.listen(PORT, () => {
-            clientSocket = Client(`ws://localhost:${PORT}`);
-            io.on('connection', (socket) => {
-                serverSocket = socket;
-            });
-            clientSocket.on('connect', done);
-        });
+        io.listen(PORT);
+        console.log('WebSocket server is running');
+        io.on('connection', handleConnection);
+        done();
     });
 
     afterAll(() => {
         io.close();
+    });
+
+    beforeEach((done) => {
+        clientSocket = Client(`ws://localhost:${PORT}`);
+        clientSocket.on('connect', done);
+    });
+
+    afterEach(() => {
         clientSocket.close();
     });
 
-    test('should handle login event', (done) => {
+    test('should handle login event admin', (done) => {
         clientSocket.emit('login', { userId: 'user1', connectionType: 'admin' });
-
         clientSocket.on('message', (data: { status: number }) => {
-            expect(data.status).toBe(200);
+            expect(data).toEqual({ status: 200 });
+            done();
+        });
+    });
+
+    test('should handle login event mobile', (done) => {
+        clientSocket.emit('login', { userId: 'tamas', connectionType: 'mobile' });
+        clientSocket.on('message', (data: { status: number }) => {
+            expect(data).toEqual({ status: 200 });
+            done();
+        });
+    });
+
+    test('should handle login event error(Invalid request)', (done) => {
+        const datas = [{ userId: 'user1' }, { connectionType: 'admin' }, {}];
+        datas.forEach((loginData) => {
+            clientSocket.emit('login', loginData);
+            clientSocket.on('message', (data: any) => {
+                    expect(data).toEqual({ status: 400, error: 'Invalid request' });
+                });
+            });
+        done();
+    });
+
+    test('should handle login event error(Invalid connection type)', (done) => {
+        clientSocket.emit('login', { userId: 'user1', connectionType: 'invalid' });
+        clientSocket.on('message', (data: any) => {
+            expect(data).toEqual({ status: 400, error: 'Invalid connection type' });
             done();
         });
     });
